55946bf6603edb92c727bf21ec5e088d11db19c7
Manish <manishkolluru3024@icloud.com>
2026-09-19 14:37:33 +0530

    Pallindrome_Number c program

---
+++ Pallindrome_Number.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, original, reverse = 0, remainder;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    original = n;
+
+    while (n != 0)
+    {
+        remainder = n % 10;
+        reverse = reverse * 10 + remainder;
+        n = n / 10;
+    }
+
+    if (original == reverse)
+    {
+        printf("%d is a palindrome number.\n", original);
+    }
+    else
+    {
+        printf("%d is not a palindrome number.\n", original);
+    }
+
+    return 0;
+}
